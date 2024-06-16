3942a2193a56abaf47b122c180454ede9f96e0f2
K-Murali <kmuralidhar888@gmail.com>
2024-06-16 23:14:35 +0530

    after imgaes

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -4,6 +4,12 @@ const User = require("../models/Login");
 exports.savetour = async (req, res) => {
   try {
     const user = await User.findById(req.body.userid);
+    if(user.save.includes(req.body.tourid)){
+      return res.status(200).json({
+        status: "fail",
+        message: "Already saved",
+      });
+    }
     const newuser = await User.findOneAndUpdate(
       { _id: req.body.userid },
       { saved: user.saved.concat(req.body.tourid) },
